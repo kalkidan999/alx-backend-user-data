5f67c5b89a317d067b5bba45f214883dc4a2b91a
kalkidan999 <kalkidandemes99@gmail.com>
2022-01-08 04:15:22 -0800

    check pwd valididty

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -18,3 +18,19 @@ def hash_password(password: str) -> bytes:
     hashed_val = bcrypt.hashpw(encoded, bcrypt.gensalt())
 
     return hashed_val
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''
+       is_valid function expects 2 arguments and
+        returns a boolean.
+       Arguments:hashed_password: bytes type
+                password: string type
+       Use bcrypt to validate that the provided password matches the hashed
+       password.
+    '''
+    valid = False
+    pass_encoded = password.encode()
+    if bcrypt.checkpw(pass_encoded, hashed_password):
+        valid = True
+    return valid
