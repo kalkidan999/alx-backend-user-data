57af576c2d7d1c19f1c664560dbf10a24804deb0
kalkidan999 <kalkidandemes99@gmail.com>
2022-01-17 11:11:09 -0800

    added find functio

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -36,3 +38,14 @@ class DB:
         self._session.add(new)
         self._session.commit()
         return new
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Returns first row found in users table based on keyword args """
+
+        try:
+            record = self._session.query(User).filter_by(**kwargs).first()
+        except TypeError:
+            raise InvalidRequestError
+        if record is None:
+            raise NoResultFound
+        return record
