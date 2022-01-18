9a43b8d91361fe8a64e7f593da9055dc28133179
kalkidan999 <kalkidandemes99@gmail.com>
2022-01-18 12:40:33 -0800

    registering

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,9 +4,13 @@ Basic Flask app
 """
 
 
+import email
 from flask import Flask, jsonify
 from flask import request
+
+from auth import Auth
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -14,6 +18,22 @@ def get_reuest():
     """get req"""
     data = jsonify(message='Bienvenue')
     return data
+
+
+@app.route('/', methods=['POST'], strict_slashes=False)
+def users():
+    """endpt to register users"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        new = Auth.register_user(email, password)
+        if new is not None:
+            data = jsonify({"email": new.email,
+                            "message": "user created"})
+            return data
+    except ValueError:
+        return jsonify({"message": "email already registered"
+                        }), 400
 
 
 if __name__ == "__main__":
